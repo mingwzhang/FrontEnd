151d649929a4e90eecb1f17c0e60beb818f49c84
Mingwei Zhang <mingw.zhang123@gmail.com>
2024-04-15 13:41:22 -0400

    On to js

---
+++ JS/BMI.js
@@ -0,0 +1,5 @@
+let height = 50
+let mass = 100
+let bmi = mass / height
+
+console.log(height)
---
+++ JS/Currency.js
@@ -0,0 +1,11 @@
+let yuan = 560
+let yen = 2455
+let won = 3280
+
+let usd = (yuan/7.24) + (yen/154.25) + (won/1389.26)
+
+console.log("What do you have left in yuan?", yuan);
+console.log("What do you have left in yen?", yen);
+console.log("What do you have left in won?", won);
+
+console.log(usd);
