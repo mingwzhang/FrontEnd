3968addadb0b91982579c31e06de07296cfc2c33
mingwzhang <mingw.zhang123@gmail.com>
2024-06-15 16:47:27 -0400

    js certificate complete

---
+++ JS/checkpoint/fizz_buzz.js
@@ -0,0 +1,13 @@
+const input = 15;
+
+for (let x = 1; x <= input; x++) {
+  if ((x % 3 == 0) && (x % 5 == 0)) {
+    console.log("FizzBuzz");
+  } else if (x % 3 == 0) {
+    console.log("Fizz");
+  } else if (x % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(x);
+  }
+}
